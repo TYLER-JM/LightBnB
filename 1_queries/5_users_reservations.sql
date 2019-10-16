d5a41ef845d0330175a84531f34eb4da84f98cb9
Tyler <t.mar92@gmail.com>
2019-10-16 18:40:16 +0000

    old implementation commented out for learning purposes

--- 1_queries/5_users_reservations.sql
+++ 1_queries/5_users_reservations.sql
@@ -11,7 +11,9 @@ LIMIT 10;
 
 --OLD IMPLEMENTAION--
 --AVG DID NOT COME OUT WITH THE CORRECT NUMBER--
+--TESTING--
 
+--WRONG--
 -- SELECT title, cost_per_night, start_date, AVG(rating)
 -- FROM property_reviews
 -- JOIN properties ON (property_id = properties.id)
@@ -19,8 +21,19 @@ LIMIT 10;
 -- WHERE reservations.guest_id = 1
 -- AND end_date < now()::date
 -- GROUP BY title, cost_per_night, start_date
--- ORDER BY start_date;
+-- ORDER BY start_date
+-- LIMIT 10;
 
+--RIGHT--
+-- SELECT title, cost_per_night, start_date, AVG(rating)
+-- FROM reservations
+-- JOIN properties ON (reservations.property_id = properties.id)
+-- JOIN property_reviews ON (properties.id = property_reviews.property_id)
+-- WHERE reservations.guest_id = 1
+-- AND end_date < now()::date
+-- GROUP BY title, cost_per_night, start_date
+-- ORDER BY start_date
+-- LIMIT 10;
 
 --CODE TO HELP SOLVE--
 -- SELECT AVG(rating), title
